27b2414e5acca029ae64c1fb50b9dad63c7d8058
Shun <82525274+Sakamoto-Shun@users.noreply.github.com>
2022-10-13 04:58:46 +0900

    nes.css 導入

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,27 @@
 import Footer from '@/components/base/Footer';
 import Header from '@/components/base/Header';
 import type { NextPage } from 'next';
-import Head from 'next/head';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
   return (
     <div>
       <Header />
-      <main>
-        <h1>Hello Github Pages</h1>
-      </main>
-      <Footer />
+      <div className="relative mx-auto">
+        <div className="absolute top-0 left-0 z-[-1]">
+          <Image src="images/home.png" width="1440" height="1024" />
+        </div>
+
+        <div className="absolute top-[180px] left-[10%]">
+          <section className="message -right">
+            <div className="nes-balloon from-left">
+              <p>よぉ</p>
+            </div>
+          </section>
+          <i className="nes-octocat animate"></i>
+        </div>
+      </div>
+      {/* <Footer /> */}
     </div>
   );
 };
